7e5a29a4c9348e7b20bdb5532cd49704110fc253
francescferremagrinya-coder <francesc.ferre.magrinya@gmail.com>
2026-09-14 18:54:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Gestiona cache offline i instal·lació PWA
 // ════════════════════════════════════════════════════════
 
-const CACHE_NOM = 'medalles-v3';
+const CACHE_NOM = 'medalles-v4';
 const CACHE_FITXERS = [
   './',
   './index.html',
